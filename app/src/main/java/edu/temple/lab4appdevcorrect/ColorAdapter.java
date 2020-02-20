4d9855f737f3a07af88aad33e56eab074ffecc55
Genevieve <genevievepc47@gmail.com>
2020-02-20 10:04:02 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/lab4appdevcorrect/ColorAdapter.java
@@ -0,0 +1,49 @@
+package edu.temple.lab4appdevcorrect;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class ColorAdapter extends BaseAdapter {
+
+    Context context;
+    String[] colors;
+
+    public ColorAdapter(Context context,String[] colors) {
+        super();
+        this.context = context;
+        this.colors = colors;
+    }
+
+    @Override
+    public int getCount() {
+        return colors.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return colors[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view;
+
+        view = View.inflate(context, R.layout.row, null);
+        final TextView textView = (TextView) view.findViewById(R.id.colorRow);
+        textView.setText(colors[position]);
+
+        textView.setBackgroundColor(Color.parseColor(colors[position]));
+        return view;
+
+    }
+}
---
+++ app/src/main/java/edu/temple/lab4appdevcorrect/PaletteActivity.java
@@ -0,0 +1,47 @@
+package edu.temple.lab4appdevcorrect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class PaletteActivity extends AppCompatActivity {
+
+    String[] colors = {"red", "blue", "green", "gray", "cyan", "magenta", "yellow", "lightgray", "darkgray", "aqua", "fuchsia"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+        mySpinner.setAdapter(new ColorAdapter(PaletteActivity.this, colors));
+
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                //change color of layout background
+                findViewById(R.id.PaletteActivity).setBackgroundColor(Color.parseColor(colors[position]));
+
+                //change color of selected text view to white
+                TextView textView = (TextView)parent.getChildAt(0);
+                textView.setBackgroundColor(Color.parseColor("white"));//CRASHES PROGRAM
+
+
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+}
